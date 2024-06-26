6c74beb3a88b9af35d05fa1ccda01db885f8ac20
Swoyam Siddharth Nayak <swoyamsiddharthnayak@gmail.com>
2024-06-26 19:52:17 +0530

    changed navbar

--- components/navbar/index.jsx
+++ components/navbar/index.jsx
@@ -85,14 +85,22 @@ export default function BaseNavbar() {
                 <NavDropdown.Item href="/about/collaborations">
                   Collaborations
                 </NavDropdown.Item>
-                <NavDropdown.Item href="/about/team">
+                {/* <NavDropdown.Item href="/about/team">
                   Placement Team
-                </NavDropdown.Item>
+                </NavDropdown.Item> */}
                 {/* <NavDropdown.Divider /> */}
                 {/* <NavDropdown.Item href="/faculty.pdf" className="">
                   Download Faculty Profile
                 </NavDropdown.Item> */}
               </NavDropdown>
+              <Nav.Item>
+                <Nav.Link
+                  href="/about/team"
+                  className="fw-bold"
+                >
+                  Team
+                </Nav.Link>
+              </Nav.Item>
               <Nav.Item>
                 <Nav.Link
                   href="https://sites.google.com/iiit-bh.ac.in/alumni"
